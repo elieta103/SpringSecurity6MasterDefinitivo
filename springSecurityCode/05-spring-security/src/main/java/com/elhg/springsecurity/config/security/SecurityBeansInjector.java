5363444ef6f71adad2dd81cb0466dba0000faba1
Eliel Herrera Gress <elieta103@gmail.com>
2024-02-20 15:15:17 -0600

    05 Method Security: Autorización basada en aseguramiento de métodos Controller

---
+++ springSecurityCode/05-spring-security/src/main/java/com/elhg/springsecurity/config/security/SecurityBeansInjector.java
@@ -0,0 +1,53 @@
+package com.elhg.springsecurity.config.security;
+
+import com.elhg.springsecurity.exception.ObjectNotFoundException;
+import com.elhg.springsecurity.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+
+@Configuration
+public class SecurityBeansInjector {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> {
+            return userRepository.findByUsername(username)
+                    .orElseThrow(()-> new ObjectNotFoundException("User not found with username : "+username));
+        };
+    }
+
+
+
+}
---
+++ springSecurityCode/05-spring-security/src/main/java/com/elhg/springsecurity/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.elhg.springsecurity.service;
+
+import com.elhg.springsecurity.dto.SaveCategory;
+import com.elhg.springsecurity.persistence.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface CategoryService {
+    Page<Category> findAll(Pageable pageable);
+
+    Optional<Category> findOneById(Long categoryId);
+
+    Category createOne(SaveCategory saveCategory);
+
+    Category updateOneById(Long categoryId, SaveCategory saveCategory);
+
+    Category disableOneById(Long categoryId);
+}
