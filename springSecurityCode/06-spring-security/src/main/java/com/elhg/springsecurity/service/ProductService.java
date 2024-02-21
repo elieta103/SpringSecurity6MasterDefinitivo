a514d4c0eddd7e0d84d2c2dced971b93e98703ce
Eliel Herrera Gress <elieta103@gmail.com>
2024-02-21 11:04:50 -0600

    06 Method Security: Autorización basada en aseguramiento de métodos Services & Repository, permitAll denyAll.

---
+++ springSecurityCode/06-spring-security/src/main/java/com/elhg/springsecurity/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.elhg.springsecurity.service;
+
+
+import com.elhg.springsecurity.dto.SaveProduct;
+import com.elhg.springsecurity.persistence.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Optional;
+
+public interface ProductService {
+
+    Page<Product> findAll(Pageable pageable);
+
+    Optional<Product> findOneById(Long productId);
+
+    Product createOne(SaveProduct saveProduct);
+
+    Product updateOneById(Long productId, SaveProduct saveProduct);
+
+    Product disableOneById(Long productId);
+}
---
+++ springSecurityCode/06-spring-security/src/main/java/com/elhg/springsecurity/service/impl/ProductServiceImpl.java
@@ -0,0 +1,75 @@
+package com.elhg.springsecurity.service.impl;
+
+
+import com.elhg.springsecurity.dto.SaveProduct;
+import com.elhg.springsecurity.exception.ObjectNotFoundException;
+import com.elhg.springsecurity.persistence.entity.Category;
+import com.elhg.springsecurity.persistence.entity.Product;
+import com.elhg.springsecurity.persistence.repository.ProductRepository;
+import com.elhg.springsecurity.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Product> findOneById(Long productId) {
+        return productRepository.findById(productId);
+    }
+
+    @Override
+    public Product createOne(SaveProduct saveProduct) {
+        Product product = new Product();
+        product.setName(saveProduct.getName());
+        product.setPrice(saveProduct.getPrice());
+        product.setStatus(Product.ProductStatus.ENABLED);
+
+        // No se uso CASCADE
+        // Si no existe categoria al guardar el producto, marca una integridad referencial
+        // Si existe categoria y modifico algun dato, no se modifica porque no se uso el CASCADE
+        Category category = new Category();
+        category.setId(saveProduct.getCategoryId());
+        product.setCategory(category);
+
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateOneById(Long productId, SaveProduct saveProduct) {
+        Product productFromDB = productRepository.findById(productId)
+                        .orElseThrow( () -> new ObjectNotFoundException("Product not found with id " + productId));
+
+        productFromDB.setName(saveProduct.getName());
+        productFromDB.setPrice(saveProduct.getPrice());
+
+        Category category = new Category();
+        category.setId(saveProduct.getCategoryId());
+        productFromDB.setCategory(category);
+
+        return productRepository.save(productFromDB);
+    }
+
+    @Override
+    public Product disableOneById(Long productId) {
+        Product productFromDB = productRepository.findById(productId)
+                .orElseThrow( () -> new ObjectNotFoundException("Product not found with id " + productId));
+        productFromDB.setStatus(Product.ProductStatus.DISABLED);
+
+        return productRepository.save(productFromDB);
+    }
+}
