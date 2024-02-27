5f043310892ec5bced31f738024a994c64742491
Eliel Herrera Gress <elieta103@gmail.com>
2024-02-27 11:59:36 -0600

    12 CORS  Cross-Origin Resource Sharing

---
+++ springSecurityCode/12-spring-security/src/main/java/com/elhg/springsecurity/persistence/repository/security/JwtTokenRepository.java
@@ -0,0 +1,12 @@
+package com.elhg.springsecurity.persistence.repository.security;
+
+import com.elhg.springsecurity.persistence.entity.security.JwtToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface JwtTokenRepository extends JpaRepository<JwtToken, Long> {
+
+    Optional<JwtToken> findByToken(String jwt);
+}
